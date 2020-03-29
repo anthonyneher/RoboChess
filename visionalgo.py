0d3a294322e878d812a65c14a2640b0063318c28
anthonyneher <36486587+anthonyneher@users.noreply.github.com>
2020-03-29 16:04:24 -0400

    Update visionalgo.py

--- visionalgo.py
+++ visionalgo.py
@@ -1,45 +1,81 @@
 import cv2
 import numpy as np
 from picamera import PiCamera
+import picamera
+import picamera.array
 import time
+import chess
+import os
+import warnings
 
+blue_lower = (0,120,70)
+blue_upper = (20,255,255)
+orange_lower = (110,100,100)
+orange_upper = (120,255,255)
 
 def main():
-	
+	warnings.filterwarnings("ignore")
+	print("started")
 	camera = PiCamera()
-	#camera.rotation = 180
-	camera.capture('raw_board.jpg')
-	image = cv2.imread('raw_board.jpg')
+	camera.resolution = (2592, 1944)
+	camera.framerate = 15
+	output = np.empty((1952,2592,3), dtype=np.uint8)
+	camera.capture(output, format='rgb')
+	output = cv2.cvtColor(output,cv2.COLOR_BGR2RGB)
 	"""
 	board_size = (7,7)
-	board = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	board = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
 	#finding the inner 7x7 corners in the board
-	found, inner_corners = cv2.findChessboardCorners(image, board_size)
+	found, inner_corners = cv2.findChessboardCorners(output, board_size)
+	if(found): print("corners found")
+	else: print("corners not found")
 	"""
-	
 	#hardcoded outer corners of chess board measured using external software
-	#corners = np.zeros((4,2), dtype = "float32")
-	corners = ((23, 10), (490, 14), (490, 479), (22, 480))
-
-	board = perspective_transform(image, corners)
-
+	corners = ((517, 119), (2235, 111), (2233, 1791), (565, 1835))
+	board = perspective_transform(output, corners)
+	board = rotate(board, 90)
 	cv2.imwrite('board.jpg', board)
+	"""
+	hsv_img = cv2.cvtColor(board,cv2.COLOR_RGB2HSV)
+	blue = cv2.inRange(hsv_img, blue_lower, blue_upper)
+	cv2.imwrite('blue.jpg', blue)
+	
+	orange = cv2.inRange(hsv_img, orange_lower, orange_upper)
+	cv2.imwrite('orange.jpg',orange)
+	"""
+	positions = read_board(board)
+	print(positions)
+	
+	
+#rotates image by degrees specified in angle parameter
+def rotate(image, angle):
+	(h, w) = image.shape[:2]
+	c = (w / 2, h / 2)
+	m = cv2.getRotationMatrix2D(c, angle, 1.0)
+	new = cv2.warpAffine(image, m, (h, w))
+	return new
 	
 #returns subsquare image at specified index
 def subsquare(image, index):
 	x_size, y_size, trash = image.shape
-	step = x_size/8
-	x = (x_size/index[0])
-
-	subsquare = image[int(x_size/index[0]):int(x_size/(index[0]+1)), int(y_size/index[1]):int(y_size/(index[1]+1)), :]
+	x = x_size/8
+	y = y_size/8
+	subsquare = image[int(x*index[1]):int(x*(index[1]+1)), int(y*index[0]):int(y*(index[0]+1)), :]
 	return subsquare
+	
+#returns target window of subsquare
+def target_square(image):
+	x, y, trash = image.shape
+	target = image[int(x/4):int(x*3/4), int(y/4):int(y*3/4), :]
+	return target
 
 def read_board(board):
 	positions = np.zeros((8,8),dtype=int)
 	for x in range(8):
 		for y in range(8):
 			square = subsquare(board, (y,x))
-			positions[x][y] = classify(square)
+			target = target_square(square)
+			positions[x][y] = classify(target)
 	return positions
 
 
@@ -50,7 +86,7 @@ that are within the color range specified for blue and orange. If the pixel exis
 set to 255 and 0 otherwise. The sum of the mask values is used to determine if piece is present
 """
 def classify(square):
-	min_thresh = 6.0
+	threshold = 1000
 	hsv_img = cv2.cvtColor(square,cv2.COLOR_RGB2HSV)
 	if np.sum(cv2.inRange(hsv_img, blue_lower, blue_upper)) > threshold:
 		return 2
@@ -77,9 +113,6 @@ def perspective_transform(src_image, corners):
 	new_image = cv2.warpPerspective(src_image, matrix, (max_width, max_height))
 
 	return new_image
-
-
-
 
 
 if __name__ == '__main__':
